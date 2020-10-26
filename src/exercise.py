caced0c5178a9e95f809227c50d3a64402538473
Jaime Santos <jaimepereirasantos123@gmail.com>
2020-10-26 12:50:50 +0000

    solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,10 @@
 def main():
     #write your code below this line
+    with open("data.txt", 'r') as f:
+      lines = f.read().splitlines()
+    
+    for line in lines:
+      print(line)
 
 if __name__ == '__main__':
     main()
